1a237647baf4b26d1af2cb61411ea1ea9bc4efe8
atulverma295 <atul295@gmail.com>
2019-06-08 00:32:12 +0530

    test commit

--- controller/src/main/java/com/freedom/persons/controller/PersonController.java
+++ controller/src/main/java/com/freedom/persons/controller/PersonController.java
@@ -312,3 +312,4 @@ public class PersonController {
 		return forwardPage;
 	}
 }
+
---
+++ controller/src/main/java/com/freedom/persons/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.freedom.persons.controller;
+
+public class TestController {
+
+	@Override
+	public String toString() {
+		return "TestController []";
+	}
+
+	
+
+}
